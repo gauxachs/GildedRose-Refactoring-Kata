227c2855aecbd91c0eabb1a18349e9fe25141cdf
Jose Luis <gauxachs@gmail.com>
2019-08-25 20:40:51 +0200

    Extract methods and flip the if-else sentence

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -18,56 +18,78 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
-                if (items[i].quality > MIN_QUALITY) {
-                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        decreaseQuality(i);
+            updateQuality(i);
+
+            updateSellIn(i);
+
+            updateQualityIfSellInHasPassed(i);
+        }
+    }
+
+    private void updateQualityIfSellInHasPassed(int i) {
+        if (items[i].sellIn < 0) {
+            if (!isAgedBrie(i)) {
+                if (!isBackstagePasses(i)) {
+
+                    if (items[i].quality > MIN_QUALITY) {
+                        if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                            decreaseQuality(i);
+                        }
                     }
+                } else {
+                    items[i].quality = items[i].quality - items[i].quality;
                 }
             } else {
                 if (items[i].quality < MAX_QUALITY) {
                     increaseQuality(i);
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                increaseQuality(i);
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < MAX_QUALITY) {
-                                increaseQuality(i);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                decreaseSellIn(i);
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-                        if (items[i].quality > MIN_QUALITY) {
-                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                decreaseQuality(i);
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < MAX_QUALITY) {
-                        increaseQuality(i);
-                    }
                 }
             }
         }
     }
+
+    private void updateSellIn(int i) {
+        if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            decreaseSellIn(i);
+        }
+    }
+
+    private void updateQuality(int i) {
+        if (isAgedBrie(i) || isBackstagePasses(i)) {
+            if (items[i].quality < MAX_QUALITY) {
+                increaseQuality(i);
+
+                if (items[i].name.equals(BACKSTAGE_PASSES)) {
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < MAX_QUALITY) {
+                            increaseQuality(i);
+                        }
+                    }
+
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < MAX_QUALITY) {
+                            increaseQuality(i);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (items[i].quality > MIN_QUALITY) {
+                if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                    decreaseQuality(i);
+                }
+            }
+
+        }
+    }
+
+    boolean isAgedBrie(int i) {
+        return items[i].name.equals(AGED_BRIE);
+    }
+
+    boolean isBackstagePasses(int i) {
+        return items[i].name.equals(BACKSTAGE_PASSES);
+    }
+
 
     private void increaseQuality(int i) {
         items[i].quality = items[i].quality + 1;
